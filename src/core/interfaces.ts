b95cff97ea044caf5497cf10c4f56fddd8697c18
Nataliia Aldushyna <nataliia.aldushyna@gmail.com>
2026-05-26 13:43:56 +0200

    SOLID notification system completed

--- src/core/interfaces.ts
+++ src/core/interfaces.ts
@@ -1,16 +1,9 @@
-import { User } from '../models/User';
-
-export interface INotificationChannel {
-    send(message: string): void;
-}
+import { User } from "../models/User";
 
 export interface ILogger {
-    log(message: string): void;
+  log(message: string): void;
 }
 
-export interface INotificationService {
-    addChannel(channel: INotificationChannel): void;
-    sendEmail(user: User, message: string): void;
-    sendSMS(user: User, message: string): void;
-    sendPush(user: User, message: string): void;
+export interface INotificationChannel {
+  send(user: User, message: string): void;
 }
